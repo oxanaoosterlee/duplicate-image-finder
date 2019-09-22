a8b3fab5407194bcbdd8d7e722ca1c107ec4a0ff
Oxana Oosterlee <oxanaoosterlee@gmail.com>
2019-09-22 16:06:19 +0200

    initial commit

---
+++ image_loader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by oxana on 22-9-19.
+//
+
+#include "image_loader.h"
---
+++ image_loader.h
@@ -0,0 +1,14 @@
+//
+// Created by oxana on 22-9-19.
+//
+
+#ifndef PROJECT_IMAGE_LOADER_H
+#define PROJECT_IMAGE_LOADER_H
+
+
+class image_loader {
+        
+};
+
+
+#endif //PROJECT_IMAGE_LOADER_H
